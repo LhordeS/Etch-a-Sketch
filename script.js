bb1c0ec1838d8263fd551c4414a5045d62d5669d
LhordeS <lhorde@msn.com>
2022-08-02 19:14:39 +0900

    Create a function that generate the squares dynamically within a parent div of pre-determined size

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
-const grid = document.getElementById('grid');
+const grid = document.getElementById("grid");
+const selectBtn = document.getElementById("sqr-select-btn");
+let gridHeight = grid.getBoundingClientRect().height;
+let gridWidth = grid.getBoundingClientRect().width;
+console.log(gridWidth);
 
 function createGrid() {
-    for (let i = 0; i < 256; i++){
-        const square = document.createElement('div');
-        square.classList.add('square');
+  let selectSqrs;
+  let square;
+
+  selectBtn.addEventListener("click", () => {
+    grid.remove(square);
+    selectBtn.style.backgroundColor = "blue";
+    selectSqrs = Number(
+      prompt("Select the number of squares you would like on your grid")
+    );
+
+    if (selectSqrs <= 100) {
+      for (let i = 0; i < selectSqrs ** 2; i++) {
+        square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${
+          (gridWidth - 4 - selectSqrs * 2) / selectSqrs
+        }px`;
+        square.style.height = `${
+          (gridHeight - 4 - selectSqrs * 2) / selectSqrs
+        }px`;
+        console.log(square.style.width);
         grid.append(square);
-        square.addEventListener('mouseover', () => {
-            square.style.backgroundColor = 'red';
-        })
+        square.addEventListener("mouseover", () => {
+          square.style.backgroundColor = "red";
+        });
+      }
+    } else {
+      alert("Number too high");
     }
+  });
 }
 
 createGrid();
+
+function init() {
+  grid.remove(square);
+}
