afd19149d834a9ac79de62816ae66ec3f625a18d
LhordeS <lhorde@msn.com>
2022-08-02 11:36:31 +0900

    Complete hover effect with color change on 16X16 grid

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const grid = document.getElementById('grid');
 
 function createGrid() {
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < 256; i++){
         const square = document.createElement('div');
         square.classList.add('square');
         grid.append(square);
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        })
     }
 }
 
