cb6b151fb34ea97277ae4038c6a2cbac0d629917
LhordeS <lhorde@msn.com>
2022-08-03 09:25:35 +0900

    Add mousedown drawing functionality; separate change color function and add rainbow option; let grid reset upon entering new value

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ const grid = document.getElementById("grid");
 const selectBtn = document.getElementById("sqr-select-btn");
 let gridHeight = grid.getBoundingClientRect().height;
 let gridWidth = grid.getBoundingClientRect().width;
-console.log(gridWidth);
+
+let mouseDown;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 function createGrid() {
   let selectSqrs;
-  let square;
 
   selectBtn.addEventListener("click", () => {
-    grid.remove(square);
     selectBtn.style.backgroundColor = "blue";
+    grid.innerHTML = "";
     selectSqrs = Number(
-      prompt("Select the number of squares you would like on your grid")
+      prompt("Enter the number of squares you would like on your grid")
     );
 
     if (selectSqrs <= 100) {
       for (let i = 0; i < selectSqrs ** 2; i++) {
-        square = document.createElement("div");
+        const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = `${
           (gridWidth - 4 - selectSqrs * 2) / selectSqrs
@@ -25,11 +27,9 @@ function createGrid() {
         square.style.height = `${
           (gridHeight - 4 - selectSqrs * 2) / selectSqrs
         }px`;
-        console.log(square.style.width);
         grid.append(square);
-        square.addEventListener("mouseover", () => {
-          square.style.backgroundColor = "red";
-        });
+        square.addEventListener("mouseover", changeColor);
+        square.addEventListener("mousedown", changeColor);
       }
     } else {
       alert("Number too high");
@@ -37,8 +37,19 @@ function createGrid() {
   });
 }
 
-createGrid();
+function changeColor(e) {
+  if (e.type === "mouseover" && !mouseDown) {
+    return;
+  } else {
+    //solid color
+    // e.target.style.backgroundColor = "red";
 
-function init() {
-  grid.remove(square);
+    //random color
+    const rgb1 = Math.floor(Math.random() * 255);
+    const rgb2 = Math.floor(Math.random() * 255);
+    const rgb3 = Math.floor(Math.random() * 255);
+    e.target.style.backgroundColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+  }
 }
+
+createGrid();
